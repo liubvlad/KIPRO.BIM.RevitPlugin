59396746df09f8379f0a363d83d61447f83c127d
Vladislav <47640760+liubvlad@users.noreply.github.com>
2024-06-06 13:06:47 +0300

    Upgrade trees and famlies logic

--- FamiliesWindow.xaml.cs
+++ FamiliesWindow.xaml.cs
@@ -1,26 +1,44 @@
 ﻿namespace KIPRO.BIM.RevitPlugin
 {
+    using Autodesk.Revit.DB.Structure;
+    using Autodesk.Revit.DB;
     using Autodesk.Revit.UI;
+    using System.IO;
     using System.Windows;
     using System.Windows.Controls;
+    using System.Linq;
 
     public partial class FamiliesWindow : Window
     {
-        public FamiliesWindow()
+        private const string FileExtensionPattern = "*.rfa";
+        private string FamilyRootPath;
+        private ExternalCommandData commandData;
+        
+        public FamiliesWindow(ExternalCommandData commandData)
         {
             InitializeComponent();
+            this.commandData = commandData;
 
-            PopulateTreeView();
+            FamilyRootPath = UserSettingsHelper.Load().FamilyDirectory;
+            LoadFilesToTreeView(FamilyRootPath);
         }
 
         private void PlaceButton_Click(object sender, RoutedEventArgs e)
         {
-            if (EquipmentTreeView.SelectedItem != null)
+            if (FamiliesTreeView.SelectedItem != null)
             {
-                // TODO Логика для размещения в модели
+                var selectedItem = FamiliesTreeView.SelectedItem as TreeViewItem;
+                var filePath = (selectedItem.Tag as FileInfo)?.FullName;
 
-                var selectedItem = EquipmentTreeView.SelectedItem as TreeViewItem;
-                TaskDialog.Show("Окно выбора семейств", $"Размещаем: {selectedItem.Header}");
+                if (!string.IsNullOrEmpty(filePath) && File.Exists(filePath))
+                {
+                    var result = InputFamilyInDocument(filePath);
+                    TaskDialog.Show("Окно выбора семейств", result == Result.Succeeded ? $"Размещаем: {selectedItem.Header}" : "Ошибка при размещении семейства.");
+                }
+                else
+                {
+                    TaskDialog.Show("Окно выбора семейств", "Выберите элемент для размещения.");
+                }
                 this.Close();
             }
             else
@@ -32,106 +50,102 @@
 
         private void UpdateButton_Click(object sender, RoutedEventArgs e)
         {
-            // TODO Логика для обновления всех семейств
-            /**/ TaskDialog.Show("Окно выбора семейств", "Обновляем все семейства.");
+            LoadFilesToTreeView(FamilyRootPath);
         }
 
-        #region Заготовка дерева по умолчанию
-
-        private void PopulateTreeView()
+        private void LoadFilesToTreeView(string rootPath)
         {
-            // Автоматизация инженерных систем
-            var autoSystem = new TreeViewItem { Header = "Автоматизация инженерных систем" };
-            autoSystem.Items.Add(CreateTreeViewItem("ABB", new[] { "Компактный коммутатор 8 x 10/100BaseTX EDS-208A", "Контроллер PM5630" }));
-            autoSystem.Items.Add(CreateTreeViewItem("IECON", new[] { "Реле времени IECON-RT01", "Программируемый логический контроллер IECON-PLC20" }));
-            autoSystem.Items.Add(CreateTreeViewItem("Moxa", new[] { "Коммутатор Moxa EDS-205A", "Преобразователь MOXA NPort 5150A" }));
-            autoSystem.Items.Add(CreateTreeViewItem("Siemens", new[] { "ПЛК Siemens S7-1200", "Частотный преобразователь Siemens Sinamics V20" }));
-            EquipmentTreeView.Items.Add(autoSystem);
-
-            // Видеонаблюдение
-            var videoSurveillance = new TreeViewItem { Header = "Видеонаблюдение" };
-            videoSurveillance.Items.Add(CreateTreeViewItem("Камеры видеонаблюдения", new[] { "IP-камера Hikvision DS-2CD2143G0-I", "Аналоговая камера Dahua HAC-HDW1200TLP" }));
-            videoSurveillance.Items.Add(CreateTreeViewItem("Видеорегистраторы", new[] { "NVR Hikvision DS-7608NI-K2", "DVR Dahua XVR5108HS-X" }));
-            EquipmentTreeView.Items.Add(videoSurveillance);
-
-            // Газопроводы
-            var gasPipelines = new TreeViewItem { Header = "Газопроводы" };
-            gasPipelines.Items.Add(CreateTreeViewItem("ПГБ-50", new[] { "ПГБ-50 Вводной", "ПГБ-50 Распределительный" }));
-            gasPipelines.Items.Add(CreateTreeViewItem("ГРПШ-10", new[] { "ГРПШ-10 Вводной", "ГРПШ-10 Распределительный" }));
-            EquipmentTreeView.Items.Add(gasPipelines);
-
-            // Диспетчеризация лифтов
-            var liftDispatch = new TreeViewItem { Header = "Диспетчеризация лифтов" };
-            liftDispatch.Items.Add(new TreeViewItem { Header = "Система диспетчеризации OTIS" });
-            liftDispatch.Items.Add(new TreeViewItem { Header = "Система диспетчеризации KONE" });
-            EquipmentTreeView.Items.Add(liftDispatch);
-
-            // Домофония
-            var intercom = new TreeViewItem { Header = "Домофония" };
-            intercom.Items.Add(CreateTreeViewItem("Видеодомофоны", new[] { "Видеодомофон Commax CDV-70UM", "Видеодомофон Kocom KCV-A374SD" }));
-            intercom.Items.Add(CreateTreeViewItem("Аудиодомофоны", new[] { "Аудиодомофон Commax DP-SS", "Аудиодомофон Kocom KDP-601A" }));
-            EquipmentTreeView.Items.Add(intercom);
-
-            // ОЗДС
-            var ozds = new TreeViewItem { Header = "ОЗДС" };
-            ozds.Items.Add(CreateTreeViewItem("Система оповещения", new[] { "Система оповещения BOSCH", "Система оповещения TOA" }));
-            ozds.Items.Add(CreateTreeViewItem("Система оповещения пожарной безопасности", new[] { "Система ПБ BOSCH", "Система ПБ TOA" }));
-            EquipmentTreeView.Items.Add(ozds);
-
-            // Охранная сигнализация
-            var securitySystem = new TreeViewItem { Header = "Охранная сигнализация" };
-            securitySystem.Items.Add(new TreeViewItem { Header = "Пульт охранной сигнализации AJAX Hub" });
-            securitySystem.Items.Add(new TreeViewItem { Header = "Датчик движения AJAX MotionProtect" });
-            securitySystem.Items.Add(new TreeViewItem { Header = "Датчик открытия AJAX DoorProtect" });
-            EquipmentTreeView.Items.Add(securitySystem);
-
-            // Радиофикация
-            var radiofication = new TreeViewItem { Header = "Радиофикация" };
-            radiofication.Items.Add(CreateTreeViewItem("Усилители", new[] { "Усилитель мощности BOSCH PLE-1P120-EU", "Усилитель мощности TOA A-2240" }));
-            radiofication.Items.Add(CreateTreeViewItem("Радиоприемники", new[] { "Радиоприемник Philips OR7200", "Радиоприемник Sony XDR-S61D" }));
-            EquipmentTreeView.Items.Add(radiofication);
-
-            // Сети связи
-            var communicationNetworks = new TreeViewItem { Header = "Сети связи" };
-            communicationNetworks.Items.Add(CreateTreeViewItem("Коммутаторы", new[] { "Коммутатор Cisco SG250-26", "Коммутатор TP-Link TL-SG1024" }));
-            communicationNetworks.Items.Add(CreateTreeViewItem("Маршрутизаторы", new[] { "Маршрутизатор MikroTik hAP ac2", "Маршрутизатор Ubiquiti EdgeRouter X" }));
-            EquipmentTreeView.Items.Add(communicationNetworks);
-
-            // СКУД
-            var scud = new TreeViewItem { Header = "СКУД" };
-            scud.Items.Add(CreateTreeViewItem("Контроллеры доступа", new[] { "Контроллер доступа ZKTeco InBio460", "Контроллер доступа PERCo-CT/L04.3" }));
-            scud.Items.Add(CreateTreeViewItem("Считыватели", new[] { "Считыватель карт HID iCLASS SE", "Считыватель карт ZKTeco ProID30" }));
-            EquipmentTreeView.Items.Add(scud);
-
-            // Без привязки к производителю
-            var noManufacturer = new TreeViewItem { Header = "Без привязки к производителю" };
-            noManufacturer.Items.Add(new TreeViewItem { Header = "Электрооборудование" });
-            noManufacturer.Items.Add(new TreeViewItem { Header = "Слаботочные системы" });
-            EquipmentTreeView.Items.Add(noManufacturer);
-
-            // УГО
-            var ugo = new TreeViewItem { Header = "УГО" };
-            ugo.Items.Add(new TreeViewItem { Header = "Устройство" });
-            ugo.Items.Add(new TreeViewItem { Header = "Группа объектов" });
-            EquipmentTreeView.Items.Add(ugo);
-
-            // Марки
-            var marks = new TreeViewItem { Header = "Марки" };
-            marks.Items.Add(new TreeViewItem { Header = "Электрика" });
-            marks.Items.Add(new TreeViewItem { Header = "Отопление" });
-            EquipmentTreeView.Items.Add(marks);
-        }
-
-        private TreeViewItem CreateTreeViewItem(string header, string[] subItems)
-        {
-            var item = new TreeViewItem { Header = header };
-            foreach (var subItem in subItems)
+            FamiliesTreeView.Items.Clear();
+            var rootDirectory = new DirectoryInfo(rootPath);
+            foreach (var directory in rootDirectory.GetDirectories())
             {
-                item.Items.Add(new TreeViewItem { Header = subItem });
+                var directoryNode = new TreeViewItem { Header = directory.Name, Tag = directory };
+                FamiliesTreeView.Items.Add(directoryNode);
+                LoadFilesAndDirectories(directory, directoryNode);
             }
-            return item;
+
+            foreach (var file in rootDirectory.GetFiles(FileExtensionPattern))
+            {
+                var fileNode = new TreeViewItem { Header = file.Name, Tag = file };
+                FamiliesTreeView.Items.Add(fileNode);
+            }
         }
 
-        #endregion
+        private void LoadFilesAndDirectories(DirectoryInfo directoryInfo, TreeViewItem parentNode)
+        {
+            foreach (var directory in directoryInfo.GetDirectories())
+            {
+                var directoryNode = new TreeViewItem { Header = directory.Name, Tag = directory };
+                parentNode.Items.Add(directoryNode);
+                LoadFilesAndDirectories(directory, directoryNode);
+            }
+
+            foreach (var file in directoryInfo.GetFiles(FileExtensionPattern))
+            {
+                var fileNode = new TreeViewItem { Header = file.Name, Tag = file };
+                parentNode.Items.Add(fileNode);
+            }
+        }
+
+        private Result InputFamilyInDocument(string familyPath)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            if (!File.Exists(familyPath))
+            {
+                return Result.Failed;
+            }
+
+            Family family = null;
+            using (Transaction tx = new Transaction(doc))
+            {
+                tx.Start("Load Family");
+
+                if (!doc.LoadFamily(familyPath, out family))
+                {
+                    return Result.Failed;
+                }
+
+                tx.Commit();
+            }
+
+            if (family != null)
+            {
+                using (Transaction tx = new Transaction(doc))
+                {
+                    tx.Start("Place Family");
+
+                    // Get the FamilySymbol (type) to place
+                    var familySymbols = new FilteredElementCollector(doc)
+                        .OfClass(typeof(FamilySymbol))
+                        .OfCategoryId(family.FamilyCategoryId)
+                        .Cast<FamilySymbol>()
+                        .ToList();
+
+                    if (familySymbols.Count == 0)
+                    {
+                        return Result.Failed;
+                    }
+
+                    // For simplicity, let's just use the first type in the family
+                    FamilySymbol familySymbol = familySymbols.First();
+
+                    if (familySymbol != null && !familySymbol.IsActive)
+                    {
+                        familySymbol.Activate();
+                        doc.Regenerate();
+                    }
+
+                    XYZ location = new XYZ(0, 0, 0); // Set your desired location
+                    doc.Create.NewFamilyInstance(location, familySymbol, StructuralType.NonStructural);
+
+                    tx.Commit();
+                }
+            }
+
+            return Result.Succeeded;
+        }
     }
 }
--- KnowledgeBaseWindow.xaml.cs
+++ KnowledgeBaseWindow.xaml.cs
@@ -6,6 +6,7 @@
     using System.IO;
     using System.Windows;
     using System.Windows.Controls;
+    using System.Windows.Media;
 
     public partial class KnowledgeBaseWindow : Window
     {
@@ -36,7 +37,8 @@
                         UseShellExecute = true
                     });*/
 
-                    InitializeDocsFilesList();
+                    ///InitializeDocsFilesList();
+                    LoadFilesToTreeView(_userSettingsHelper.KnowledgeBaseDirectory);
                 }
                 else
                 {
@@ -51,6 +53,107 @@
             }
         }
 
+        private void LoadFilesToTreeView(string rootPath)
+        {
+            var rootDirectory = new DirectoryInfo(rootPath);
+            foreach (var directory in rootDirectory.GetDirectories())
+            {
+                var directoryNode = new TreeViewItem { Header = directory.Name, Tag = directory };
+                FilesTreeView.Items.Add(directoryNode);
+                LoadFilesAndDirectories(directory, directoryNode);
+            }
+
+            foreach (var file in rootDirectory.GetFiles("*.doc"))
+            {
+                var fileNode = new TreeViewItem { Header = file.Name, Tag = file };
+                FilesTreeView.Items.Add(fileNode);
+            }
+        }
+
+        private void LoadFilesAndDirectories(DirectoryInfo directoryInfo, TreeViewItem parentNode)
+        {
+            foreach (var directory in directoryInfo.GetDirectories())
+            {
+                var directoryNode = new TreeViewItem { Header = directory.Name, Tag = directory };
+                parentNode.Items.Add(directoryNode);
+                LoadFilesAndDirectories(directory, directoryNode);
+            }
+
+            foreach (var file in directoryInfo.GetFiles("*.doc"))
+            {
+                var fileNode = new TreeViewItem { Header = file.Name, Tag = file };
+                parentNode.Items.Add(fileNode);
+            }
+        }
+
+        private void FilesTreeView_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            var selectedItem = FilesTreeView.SelectedItem as TreeViewItem;
+            ResetTreeViewItemColors(FilesTreeView.Items);
+
+            if (selectedItem?.Tag is FileInfo fileInfo)
+            {
+                selectedItem.Background = Brushes.LightBlue; // Измените цвет на нужный
+                DisplayFileContent(fileInfo.FullName);
+            }
+        }
+
+        private void ResetTreeViewItemColors(ItemCollection items)
+        {
+            foreach (var item in items)
+            {
+                var treeViewItem = item as TreeViewItem;
+                if (treeViewItem != null)
+                {
+                    treeViewItem.Background = Brushes.Transparent;
+                    ResetTreeViewItemColors(treeViewItem.Items);
+                }
+            }
+        }
+
+        private void DisplayFileContent(string filePath)
+        {
+            // Здесь вы можете использовать библиотеки для чтения .doc файлов,
+            // например, Microsoft Office Interop или Aspose.Words.
+            ///ContentTextBox.Text = $"Содержимое файла: {filePath}";
+            try
+            {
+                if (File.Exists(filePath))
+                {
+                    string fileContent = File.ReadAllText(filePath);
+                    ContentTextBox.Text = fileContent;
+                }
+                else
+                {
+                    ContentTextBox.Text = string.Empty;
+                    TaskDialog.Show("База знаний", "Файл не найден.");
+                }
+            }
+            catch (Exception ex)
+            {
+                TaskDialog.Show("База знаний", $"Ошибка выбора инструкции: {ex.Message}\nОкно будет закрыто!");
+                this.Close();
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        /*
         [Obsolete("Temporery Solution")]
         private void InitializeDocsFilesList()
         {
@@ -103,3 +206,4 @@
         }
     }
 }
+*/
